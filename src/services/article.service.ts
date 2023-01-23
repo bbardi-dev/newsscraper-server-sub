01c733a78b9d4ef12111fc29d101200c13ea8e54
BBardi <76687202+bbardi-dev@users.noreply.github.com>
2023-01-23 18:24:48 +0100

    Delete src directory

--- src/services/article.service.ts
+++
@@ -1,6 +0,0 @@
-import { Prisma } from "@prisma/client";
-import prisma from "src/prisma/client";
-
-export async function findArticles(options?: Prisma.ArticleFindManyArgs) {
-  return prisma.article.findMany(options);
-}
--- src/services/scrape.service.ts
+++
@@ -1,9 +0,0 @@
-import { Article } from "@prisma/client";
-import prisma from "src/prisma/client";
-
-export async function saveScrapedArticles(scrapeData: Article[]) {
-  return prisma.article.createMany({
-    data: scrapeData,
-    skipDuplicates: true,
-  });
-}
